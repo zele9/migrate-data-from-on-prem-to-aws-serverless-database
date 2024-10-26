0a0553ae0e128bbb8c9986d616744a1242e578d8
Zele <85734555+zele9@users.noreply.github.com>
2024-10-26 09:43:00 -0400

    Add files via upload

---
+++ Script.py
@@ -0,0 +1,33 @@
+import json
+import boto3
+from decimal import Decimal
+
+def lambda_handler(event, context):
+    print(event)
+    s3_bucket = event['Records'][0]['s3']['bucket']['name']
+    json_file_name = event['Records'][0]['s3']['object']['key']
+
+    
+    s3_client = boto3.client('s3')
+    dynamodb_client = boto3.resource('dynamodb')
+    dynamodb_table = dynamodb_client.Table('LibaskiDB')
+
+    try:
+        # Get the JSON object from S3
+        response = s3_client.get_object(Bucket=s3_bucket, Key=json_file_name)
+        json_data = json.loads(response['Body'].read().decode('utf-8'), parse_float=Decimal)
+        
+        # Insert the JSON data into DynamoDB
+        with dynamodb_table.batch_writer() as batch:
+            for item in json_data:
+                batch.put_item(Item=item)
+                
+        return {
+            'statusCode': 200,
+            'body': 'Data successfully loaded into DynamoDB.'
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': f'Error: {str(e)}'
+        }
